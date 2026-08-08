db4f8ff66ffa75a40dd2dfe8a0b82547606c57b8
Ling Song <lingsong.c4@gmail.com>
2026-08-08 23:38:51 +0800

    added home skeleton

--- src/components/MainCircle.tsx
+++ src/components/MainCircle.tsx
@@ -1,7 +1,7 @@
 export function MainCircle() {
     return (
-        <div className="main-circle-container">
-            <div className="main-circle-image" aria-hidden="true" />
+        <div className="main-circle-container" aria-hidden="true">
+            <img className="main-circle-image" src="/profile.jpeg" alt="" />
         </div>
     );
 }
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,9 +1,21 @@
+import { homeDetails } from "../data/about";
+
 export function Home() {
+    const leftName = homeDetails.name.isWestern
+        ? homeDetails.name.firstName
+        : homeDetails.name.lastName;
+    const rightName = homeDetails.name.isWestern
+        ? homeDetails.name.lastName
+        : homeDetails.name.firstName;
+
     return (
         <section id="home" className="home-container" aria-labelledby="home-title">
             <div className="home-text">
-                <h1 id="home-title" className="home-title">Name</h1>
-                <p className="home-description">Role and short introduction.</p>
+                <h1 id="home-title" className="home-title">
+                    <span className="home-name-left">{leftName}</span>
+                    <span className="home-name-right">{rightName}</span>
+                </h1>
+                <p className="home-description">{homeDetails.introduction}</p>
             </div>
         </section>
     );
