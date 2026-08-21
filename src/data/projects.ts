d0dd1531d65c0d49867bba30d2df8082caef6b45
Ling Song <lingsong.c4@gmail.com>
2026-08-22 01:19:44 +0800

    added another project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,5 +11,11 @@ export const projects: Project[] = [
         name: "Modulo",
         summary: "A cross-platform academic companion for students in Singapore, combining timetable parsing, task management, Google Drive sync, and gamified study sessions.",
         href: ""
+    },
+    {
+        id: "pneurelief",
+        name: "PneuRelief",
+        summary: "A smart under-cast sock and alternating bubble patch that detect and prevent pressure ulcers before they become dangerous, while providing control via an app",
+        href: ""
     }
 ];
